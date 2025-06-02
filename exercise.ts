984c0d0659d638e51371020c3074f50b655e036d
ahmadanisas <ahmadanisasusatyo@gmail.com>
2025-06-02 11:42:46 +0700

    mengerjakan exercise Soal01 Day03

--- exercise.ts
+++ exercise.ts
@@ -1,12 +1,1 @@
-const limeit = 2;
-let ceteak: string = "";
-for (let i: number = 0; i < limit; i++) {
-    const numberToCheck = i === 0 ? 25 : 2; // First iteration checks 25, second checks 2
-    
-    if (numberToCheck % 2 === 0) {
-        cetak += `${numberToCheck} → even number\n`;
-    } else {
-        cetak += `${numberToCheck} → odd number\n`;
-    }
-}
-console.log(cetak);
+//soal 1
