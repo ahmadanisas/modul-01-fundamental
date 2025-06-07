c91f5abfddc16d640f8db6835e2b1103dc90d322
ahmadanisas <ahmadanisasusatyo@gmail.com>
2025-06-07 21:48:34 +0700

    Catatan Day03

--- Day03/loop.ts
+++ Day03/loop.ts
@@ -9,7 +9,6 @@ console.log("1. Edo");
 console.log("2. Abdi");
 console.log("3. Andre");
 
-
 /**
  * WHILE LOOP : pengulangan program yang akan selalu berjalan selama kondisi masih terpenuhi. code berpotensi tidak jalan samsek.
  * Aturan penulisan :
@@ -17,35 +16,34 @@ console.log("3. Andre");
  *      // code
  * };
  * condition : sama kaya if else ( boolean, truthly falsy, comparison, logical)
- * NOTES : jangan sampai selalu TRUE, nanti jadi infinite loop dan loop selalu nyaka
+ * NOTES : jangan sampai selalu TRUE, nanti jadi infinite loop dan loop selalu nyala
  */
 
 let count: number = 1;
 // loop 10x
-while(count <= 10){
+while (count <= 10) {
+  console.log(count);
+  count++; // count = count+1 (inkremen, count direassign)
+}
+
+while (true) {
+  count++;
+
+  if (count === 15) {
     console.log(count);
-    count++; // count = count+1 (inkremen, count direassign) 
-};
-
-while(true){
-    count++;
-
-    if(count === 15){
-        console.log(count); 
-        break; // kalo pake TRUTHLY harus pake break agar end loop
-    }
+    break; // kalo pake TRUTHLY harus pake break agar end loop
+  }
 }
 
 let isBoolean: boolean = true;
-while (isBoolean){
-    count++;
+while (isBoolean) {
+  count++;
 
-    if(count === 25){
-        console.log(count);
-        isBoolean = false; // kalo boolean TRUE harus diFALSEkan agar end loop
-    }
+  if (count === 25) {
+    console.log(count);
+    isBoolean = false; // kalo boolean TRUE harus diFALSEkan agar end loop
+  }
 }
-
 
 /**
  * DO...WHILE : kebalikan while. code jalan dulu, baru dicek. code pasti jalan minimal 1x
@@ -57,10 +55,9 @@ while (isBoolean){
 
 let counter: number = 1;
 do {
-    console.log(counter);
-    counter++;
+  console.log(counter);
+  counter++;
 } while (counter < 1);
-
 
 /**
  * FOR LOOP : digunakan ketika batas looping sudah ditentukan sejak awal (dalam hitungan angka)
@@ -68,26 +65,24 @@ do {
  * for(counter_declaration;counter_condition;counter){
  *      // code
  * }
- * 
+ *
  * - counter_declaration : variable yg mendefinisikan nilai awal dari hitungan batas looping
  * - counter_condition : kondisi untuk menentukan batas looping
  * - counter : proses perhitungan increment atau decrement
  */
 
-for(let i: number = 0; i < 10; i++){
-    console.log(i);
+for (let i: number = 0; i < 10; i++) {
+  console.log(i);
 }
 
 //latihan
 const limit = 4;
 let cetak: string = "";
-for(let i = 0; i < limit; i++){
-   if (i % 2 === 0 ) {
-        cetak += `${i} : GENAP | `;  
-    } else {
-        cetak += `${i} : GANJIL | `;
-    }
+for (let i = 0; i < limit; i++) {
+  if (i % 2 === 0) {
+    cetak += `${i} : GENAP | `;
+  } else {
+    cetak += `${i} : GANJIL | `;
+  }
 }
 console.log(cetak);
-
-
